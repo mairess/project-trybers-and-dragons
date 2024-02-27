d7c942f7083c18cd5f41b0c858a298df3a1f5ab8
mairess <trabalho.maires@gmail.com>
2024-02-27 15:55:25 -0300

    feat: abstract class Race

---
+++ src/Races/Race.ts
@@ -0,0 +1,14 @@
+abstract class Race {
+  constructor(protected _name: string, protected _dexterity: number) {
+    this._name = _name;
+    this._dexterity = _dexterity;
+    Race.createdRacesInstances();
+  }
+
+  get name(): string { return this._name; }
+  get dexterity(): number { return this._dexterity; }
+  abstract get maxLifePoints(): number;
+  static createdRacesInstances(): number { throw new Error('Not implemented'); }
+}
+
+export default Race;
