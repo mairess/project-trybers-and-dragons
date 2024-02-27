a9e715a5abca27b9d28e5910febe9fba3409a1f8
mairess <trabalho.maires@gmail.com>
2024-02-27 18:55:05 -0300

    refactor: remove duplicated method call

--- src/Races/Race.ts
+++ src/Races/Race.ts
@@ -2,7 +2,6 @@ abstract class Race {
   constructor(protected _name: string, protected _dexterity: number) {
     this._name = _name;
     this._dexterity = _dexterity;
-    Race.createdRacesInstances();
   }
 
   get name(): string { return this._name; }
