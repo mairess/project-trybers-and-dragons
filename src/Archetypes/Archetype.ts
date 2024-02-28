f1cf488319aae25874932f98892dd76f44bd5a99
mairess <trabalho.maires@gmail.com>
2024-02-28 14:30:57 -0300

    fix: modifiers visibilities

--- src/Archetypes/Archetype.ts
+++ src/Archetypes/Archetype.ts
@@ -1,9 +1,9 @@
 import { EnergyType } from '../Energy';
 
 abstract class Archetype {
-  private _name: string;
-  private _special: number;
-  private _cost: number;
+  protected _name: string;
+  protected _special: number;
+  protected _cost: number;
 
   constructor(name: string) {
     this._name = name;
