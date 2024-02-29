64cd7bbaa273edee7161d74b456ccff0e8e07f84
mairess <trabalho.maires@gmail.com>
2024-02-29 14:05:13 -0300

    feat: interface SimpleFighter

---
+++ src/Fighter/Fighter.ts
@@ -0,0 +1,12 @@
+import Energy from '../Energy';
+import SimpleFighter from './SimpleFighter';
+
+interface Fighter extends SimpleFighter {
+  defense: number,
+  energy?: Energy,
+  attack(enemy: Fighter): void;
+  special?(enemy: Fighter): void;
+  levelUp(): void;
+}
+
+export default Fighter;
