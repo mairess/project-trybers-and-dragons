439777547a2a696f06a2af94f58ad539821fa81d
mairess <trabalho.maires@gmail.com>
2024-03-01 10:43:40 -0300

    feat: class Dragon

--- src/Monster.ts
+++ src/Monster.ts
@@ -1,8 +1,8 @@
 import Fighter, { SimpleFighter } from './Fighter';
 
 class Monster implements SimpleFighter {
-  private _lifePoints: number;
-  private _strength: number;
+  protected _lifePoints: number;
+  protected _strength: number;
   constructor() {
     this._lifePoints = 85;
     this._strength = 63;
